a9baf613e003226ee4ba85aa4cf0ccff89184ec5
Oleksandr Dubovenko <o.dubovenko@viseven.com>
2024-05-07 11:48:21 +0300

    use sync map

--- commands/del.go
+++ commands/del.go
@@ -7,6 +7,7 @@ import (
 func (c *Command) Del(keys []string) string {
 	for _, k := range keys {
 		s.Store.Del(k)
+		s.ExStore.Del(k)
 	}
 	return "+OK\r\n"
 }
--- store/store.go
+++ store/store.go
@@ -5,52 +5,51 @@ import (
 )
 
 type StoreMap struct {
-	data map[string]any
-	mu   sync.RWMutex
+	data sync.Map
 }
 
 var Store = &StoreMap{
-	data: map[string]any{},
+	data: sync.Map{},
 }
 
 func (s *StoreMap) Get(key string) string {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-	if val, ok := s.data[key].(string); ok {
+	val, exists := s.data.Load(key)
+	if !exists {
+		return ""
+	}
+	if val, ok := val.(string); ok {
 		return val
 	}
 	return ""
 }
 
 func (s *StoreMap) Set(key string, val string) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	s.data[key] = val
+	s.data.Store(key, val)
 }
 
 func (s *StoreMap) Del(key string) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	delete(s.data, key)
+	s.data.Delete(key)
 }
 
 func (s *StoreMap) GetField(key string, field string) string {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-	if hash, exists := s.data[key].(map[string]string); exists {
+	hash, exists := s.data.Load(key)
+	if !exists {
+		return ""
+	}
+	if hash, ok := hash.(map[string]string); ok {
 		return hash[field]
 	}
 	return ""
 }
 
 func (s *StoreMap) SetField(key string, field string, val string) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	if hash, exists := s.data[key].(map[string]string); exists {
+	hash, exists := s.data.Load(key)
+	if !exists {
+		hash := make(map[string]string)
 		hash[field] = val
-	} else {
-		hash = make(map[string]string)
+		s.data.Store(key, hash)
+	}
+	if hash, ok := hash.(map[string]string); ok {
 		hash[field] = val
-		s.data[key] = hash
 	}
 }
